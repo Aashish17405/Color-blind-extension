e6ee059db2e1d95cbd38c1f07ac7c9c21dea5c27
Aashish17405 <aashish17405@gmail.com>
2024-10-22 23:10:11 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import logo from './logo.png'
 
 function App() {
 
   return (
     <>
       <div className='m-10 mx-20'>
-        <h1 className=' text-2xl'>Color Blind helper</h1>
+        <img
+          src={chrome.runtime.getURL(logo)}
+          className='App-logo'
+          alt='logo'
+        />
+        <h1 className='text-2xl'>Color Blind helper</h1>
       </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = document.createElement("div");
+root.id = "crx-root";
+document.body.appendChild(root);
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
